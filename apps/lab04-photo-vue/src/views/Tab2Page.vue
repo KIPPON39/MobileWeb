dda3cf680eda984a75b7277bb9be72c0732bb270
KIPPON39 <kurakuri2104@gmail.com>
2026-01-11 23:12:20 +0700

    Lab4

--- apps/lab04-photo-vue/src/views/Tab2Page.vue
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -10,29 +10,45 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Photo Gallery</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    </ion-content>
+      <!-- Photo Grid -->
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
-    <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="camera"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
+      <!-- Floating Action Button -->
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
   </ion-page>
 </template>
 
-
 <script setup lang="ts">
-import { camera } from 'ionicons/icons';
-import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+
 
 // CHANGE: Add `usePhotoGallery` import
 import { usePhotoGallery } from '@/composables/usePhotoGallery';
-
-// CHANGE: Destructure `addNewToGallery` from `usePhotoGallery()
-const { addNewToGallery } = usePhotoGallery();
+// CHANGE: Add `photos` array to destructure from `usePhotoGallery()`
+const { photos, addNewToGallery } = usePhotoGallery();
 </script>
