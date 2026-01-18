3f59928377fcfae2c3e1446e2007abef3a91b269
KIPPON39 <kurakuri2104@gmail.com>
2026-01-18 21:05:36 +0700

    Lab05 Ionic React

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,70 @@
+// CHANGE: Add the following import
+import { camera } from 'ionicons/icons';
+// CHANGE: Update the following import
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/react';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+// CHANGE: Remove or comment out `ExploreContainer`
+// import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="small">Lab 05 - โดย วิสิษฎ์ ศรีอดิศักดิ์ รหัส 663380234-8</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="small">Lab 05 - โดย วิสิษฎ์ ศรีอดิศักดิ์ รหัส 663380234-8</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        {/* CHANGE: Add a grid component to display the photos */}
+        <IonGrid>
+          <IonRow>
+            {/* CHANGE: Create a new column and image component for each photo */}
+            {photos.map((photo) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+
+        {/* CHANGE: Add the floating action button */}
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          {/* CHANGE: Add a click event listener to the floating action button */}
+          <IonFabButton onClick={() => addNewToGallery()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+
+export default Tab2;
+
